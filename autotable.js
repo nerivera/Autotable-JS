d80a386f8c376a1747066e9f198eb87e88d35a0a
Nilo E Rivera <47746418+nerivera@users.noreply.github.com>
2020-04-15 21:23:30 -0400

    Update autotable.js

--- autotable.js
+++ autotable.js
@@ -1,9 +1,9 @@
 function genTable(rows, cols, class, enum=true, write=false) {
-  let res = "<table>\n  <tbody>\n";
+  let res = "<table class=\"" + class + "\">\n  <tbody class=" + class + "\">\n";
   for(let r = 0; r < rows; r ++) {
-    res += "    <tr>\n";
+    res += "    <tr class=\"" + class + "\">\n";
     for(let c = 0; c < cols; c ++) {
-      res += "      <td class=" + class + (enum ? " " + (r * cols + c).toString() : "") + "\"></td>\n";
+      res += "      <td class=\"" + class + (enum ? " " + (r * cols + c).toString() : "") + "\"></td>\n";
     }
     res += "    </tr>\n";
   }
